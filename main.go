bce8c35e27be429fc9f06a691ca654aadc5cd0cb
Aleksandr Stukalov <stukalovsaleksandrs@gmail.com>
2023-03-20 07:54:39 +0200

    Added camera rotation

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"image/color"
 	"log"
+	"math"
 	"time"
 
 	v "github.com/34thSchool/vectors"
@@ -20,13 +21,12 @@ func CentralProjection(a v.Vec, k float64) v.Vec {
 		X: a.X / a.Z * k,
 		Y: a.Y / a.Z * k,
 	}
-
 }
 
 func DrawLine(screen *ebiten.Image, g *game, a, b v.Vec, clr color.Color) {
 	halfWidth, halfHeight := float64(screenWidth/2), float64(screenHeight/2)
 
-	a, b = v.Sub(a, g.Cam.Pos), v.Sub(b, g.Cam.Pos)
+	a, b = WorldToCamera(&g.Cam, a), WorldToCamera(&g.Cam, b)
 
 	k := float64(250)
 	a, b = CentralProjection(a, k), CentralProjection(b, k)
@@ -54,29 +54,60 @@ func (c *Cube) Draw(screen *ebiten.Image, g *game, clr color.Color) {
 }
 
 type Camera struct {
-	Pos   v.Vec
-	Speed float64
+	Pos           v.Vec
+	Rad           v.Rotator
+	MovementSpeed float64
+	RotationSpeed float64
 }
 
-func (Cam *Camera) ProcessMovement(dt float64) {
-	if ebiten.IsKeyPressed(ebiten.KeyD) {
-		Cam.Pos.X += Cam.Speed * dt
+func (Cam *Camera) Update(dt float64) {
+	// Movement:
+	if ebiten.IsKeyPressed(ebiten.KeyArrowRight) {
+		Cam.Pos.X += Cam.MovementSpeed * dt
 	}
-	if ebiten.IsKeyPressed(ebiten.KeyA) {
-		Cam.Pos.X -= Cam.Speed * dt
+	if ebiten.IsKeyPressed(ebiten.KeyArrowLeft) {
+		Cam.Pos.X -= Cam.MovementSpeed * dt
+	}
+	if ebiten.IsKeyPressed(ebiten.KeySpace) {
+		Cam.Pos.Y += Cam.MovementSpeed * dt
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyControl) {
+		Cam.Pos.Y -= Cam.MovementSpeed * dt
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyArrowUp) {
+		Cam.Pos.Z += Cam.MovementSpeed * dt
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyArrowDown) {
+		Cam.Pos.Z -= Cam.MovementSpeed * dt
+	}
+
+	// Rotation:
+	if ebiten.IsKeyPressed(ebiten.KeyQ) {
+		Cam.Rad.Z += Cam.RotationSpeed * dt
 	}
 	if ebiten.IsKeyPressed(ebiten.KeyE) {
-		Cam.Pos.Y += Cam.Speed * dt
+		Cam.Rad.Z -= Cam.RotationSpeed * dt
 	}
-	if ebiten.IsKeyPressed(ebiten.KeyQ) {
-		Cam.Pos.Y -= Cam.Speed * dt
+	if ebiten.IsKeyPressed(ebiten.KeyA) {
+		Cam.Rad.Y -= Cam.RotationSpeed * dt
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyD) {
+		Cam.Rad.Y += Cam.RotationSpeed * dt
 	}
 	if ebiten.IsKeyPressed(ebiten.KeyW) {
-		Cam.Pos.Z += Cam.Speed * dt
+		Cam.Rad.X -= Cam.RotationSpeed * dt
 	}
 	if ebiten.IsKeyPressed(ebiten.KeyS) {
-		Cam.Pos.Z -= Cam.Speed * dt
+		Cam.Rad.X += Cam.RotationSpeed * dt
 	}
+}
+
+func WorldToCamera(Cam *Camera, a v.Vec) v.Vec {
+	a = v.Sub(a, Cam.Pos)
+	a.RotateX(-Cam.Rad.X)
+	a.RotateY(-Cam.Rad.Y)
+	a.RotateZ(-Cam.Rad.Z)
+	return a
 }
 
 type game struct {
@@ -96,14 +127,19 @@ func NewGame() *game {
 					{200, 200, 400},   // NearTopRight
 					{200, -200, 400},  // NearBottomRight
 
-					{-200, -200, 800}, // FarBottomLeft
-					{-200, 200, 800},  // FarTopLeft
-					{200, 200, 800},   // FarTopRight
-					{200, -200, 800},  // FarBottomRight
+					{-200, -200, 700}, // FarBottomLeft
+					{-200, 200, 700},  // FarTopLeft
+					{200, 200, 700},   // FarTopRight
+					{200, -200, 700},  // FarBottomRight
 				},
 			},
 		},
-		Camera{v.Vec{0, 0, 0}, 1},
+		Camera{
+			v.Vec{X: 0, Y: 0, Z: 0},
+			v.Rotator{X: 0, Y: 0, Z: 0},
+			1,
+			math.Pi / 720,
+		},
 		0,
 		ebiten.NewImage(screenWidth, screenHeight),
 	}
@@ -114,7 +150,7 @@ func (g *game) Update() error {
 	dt := float64(time.Now().UnixMilli() - g.prevFrameTime)
 	g.prevFrameTime = time.Now().UnixMilli()
 
-	g.Cam.ProcessMovement(dt)
+	g.Cam.Update(dt)
 
 	return nil
 }
