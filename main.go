990aca16b61397b9c774b6e3137621397321dd91
Vladimir Satinov <vovaumni@gmail.com>
2023-03-22 20:09:28 +0200

    done

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"image/color"
 	"log"
+	"math"
 	"math/rand"
 	"time"
 
@@ -21,44 +22,95 @@ type Point struct {
 
 type Game struct {
 	width, height int
-	dots          []Point
+	dots          []*Point
 	camera        Point
+	cameraAngleX  float64
+	cameraAngleY  float64
+	cameraAngleZ  float64
 }
 
 func (g *Game) Layout(outWidth, outHeight int) (w, h int) {
 	return g.width, g.height
 }
 
+func cameraProject(p *Point, camera Point) *Point {
+	return &Point{p.x - camera.x, p.y - camera.y, p.z - camera.z}
+}
+func (g *Game) CameraRotateY(p *Point, angle float64) *Point {
+	p.x = p.x*math.Cos(angle) + p.z*math.Sin(angle)
+	p.z = -p.x*math.Sin(angle) + p.z*math.Cos(angle)
+	return p
+}
+func (g *Game) CameraRotateX(p *Point, angle float64) *Point {
+	p.y = p.y*math.Cos(angle) - p.z*math.Sin(angle)
+	p.z = p.y*math.Sin(angle) + p.z*math.Cos(angle)
+	return p
+}
+
+func (g *Game) CameraRotateZ(p *Point, angle float64) *Point {
+	p.x = p.x*math.Cos(angle) - p.y*math.Sin(angle)
+	p.y = p.x*math.Sin(angle) + p.y*math.Cos(angle)
+	return p
+}
+
 func (g *Game) Update() error {
+	g.camera = Point{0, 0, 0}
+	g.cameraAngleX = 0
+	g.cameraAngleY = 0
+	g.cameraAngleZ = 0
 	if ebiten.IsKeyPressed(ebiten.KeyA) {
-		g.camera.x--
+		g.camera.x = 1
 	}
 	if ebiten.IsKeyPressed(ebiten.KeyD) {
-		g.camera.x++
+		g.camera.x = -1
 	}
 	if ebiten.IsKeyPressed(ebiten.KeyW) {
-		g.camera.z--
+		g.camera.z = 1
 	}
 	if ebiten.IsKeyPressed(ebiten.KeyS) {
-		g.camera.z++
+		g.camera.z = -1
 	}
 	if ebiten.IsKeyPressed(ebiten.KeySpace) {
-		g.camera.y--
+		g.camera.y = 1
 	}
 	if ebiten.IsKeyPressed(ebiten.KeyShiftLeft) {
-		g.camera.y++
+		g.camera.y = -1
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyArrowLeft) {
+		g.cameraAngleY = math.Pi / 360
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyArrowRight) {
+		g.cameraAngleY = -math.Pi / 360
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyArrowUp) {
+		g.cameraAngleX = math.Pi / 360
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyArrowDown) {
+		g.cameraAngleX = -math.Pi / 360
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyPageUp) {
+		g.cameraAngleZ = math.Pi / 360
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyPageDown) {
+		g.cameraAngleZ = -math.Pi / 360
+	}
+	for i := range g.dots {
+		g.dots[i] = cameraProject(g.dots[i], g.camera)
+		g.dots[i] = g.CameraRotateX(g.dots[i], g.cameraAngleX)
+		g.dots[i] = g.CameraRotateY(g.dots[i], g.cameraAngleY)
+		g.dots[i] = g.CameraRotateZ(g.dots[i], g.cameraAngleZ)
 	}
 	return nil
 }
 
-func (g *Game) DrawLine(screen *ebiten.Image, a, b Point) {
+func (g *Game) DrawLine(screen *ebiten.Image, a, b *Point) {
 	z1 := a.z + 500
 	z2 := b.z + 500
 	x1 := a.x / z1
 	y1 := a.y / z1
 	x2 := b.x / z2
 	y2 := b.y / z2
-	ebitenutil.DrawLine(screen, x1*(500-g.camera.z)+float64(g.width)/2-g.camera.x, y1*(500-g.camera.z)+float64(g.height)/2-g.camera.y, x2*(500-g.camera.z)+float64(g.width)/2-g.camera.x, y2*(500-g.camera.z)+float64(g.height)/2-g.camera.y, color.White)
+	ebitenutil.DrawLine(screen, x1*500+float64(g.width)/2, y1*500+float64(g.height)/2, x2*500+float64(g.width)/2, y2*500+float64(g.height)/2, color.White)
 
 }
 
@@ -83,7 +135,7 @@ func NewGame(width, height int) *Game {
 	return &Game{
 		width:  width,
 		height: height,
-		dots: []Point{
+		dots: []*Point{
 			{-100, -100, -100},
 			{-100, 100, -100},
 			{100, 100, -100},
