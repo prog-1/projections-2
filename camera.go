32589317584855895efc0c777c002e8644f39e14
vay4ie <vay4ie@gmail.com>
2023-03-26 16:13:18 +0300

    Implemented rotation
    Made camera rotation with some artifacts

--- camera.go
+++ camera.go
@@ -3,7 +3,7 @@ package main
 import (
 	"image/color"
 	"log"
-	"fmt"
+	"math"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
@@ -17,12 +17,11 @@ const (
 )
 
 type Game struct {
-	width, height int //screen width and height
-	//global variables
-	cp        point    //cube central point
-	cube     [8]point // cube points
-	edges      [][2]int // cube edges
-	cam camera//camera
+	width, height int      //screen width and height
+	cube          [8]point // cube points
+	edges         [][2]int // cube edges
+	cp            point    // cube center point
+	cam           camera   //camera
 }
 
 type point struct {
@@ -30,14 +29,48 @@ type point struct {
 }
 
 type camera struct {
-	x,y,z float64
+	pos point //camera position
+	rot point //camera rotation angle
 }
 
 //---------------------------Update-------------------------------------
 
 func (g *Game) Update() error {
-	if ebiten.IsKeyPressed(ebiten.KeyArrowRight){
-		g.cam.moveRight()
+	if ebiten.IsKeyPressed(ebiten.KeyD) {
+		g.cam.pos.x += 7
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyA) {
+		g.cam.pos.x -= 7
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyE) {
+		g.cam.pos.y -= 7
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyQ) {
+		g.cam.pos.y += 7
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyW) {
+		g.cam.pos.z += 7
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyS) {
+		g.cam.pos.z -= 7
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyNumpad8) { // - X
+		g.cam.rot.x -= math.Pi / 200
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyNumpad2) { // + X
+		g.cam.rot.x += math.Pi / 200
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyNumpad4) { // - Y
+		g.cam.rot.y += math.Pi / 200
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyNumpad6) { // + Y
+		g.cam.rot.y -= math.Pi / 200
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyNumpad9) { // + Z
+		g.cam.rot.z += math.Pi / 200
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyNumpad7) { // - Z
+		g.cam.rot.z -= math.Pi / 200
 	}
 	return nil
 }
@@ -45,40 +78,97 @@ func (g *Game) Update() error {
 //---------------------------Draw-------------------------------------
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	//cube drawing without normals and stuff	
-	for i := range g.edges {
+
+	//cube line draw
+	for i := range g.edges { //for all cube edges
 		g.drawLine(screen, g.cube[g.edges[i][0]], g.cube[g.edges[i][1]])
 	}
+
 }
 
 //line draw with central projection
 func (g *Game) drawLine(screen *ebiten.Image, a, b point) {
 
-	//central projection
-	k := float64(400)
-	cproj(&a, g.cp, k)
-	cproj(&b, g.cp, k)
-
+	a = updatePoint(a, g.cam, g.cp)
+	b = updatePoint(b, g.cam, g.cp)
 	//draw function
-	ebitenutil.DrawLine(screen, g.cp.x+a.x, g.cp.y+a.y, g.cp.x+b.x, g.cp.y+b.y, color.White)
+	ebitenutil.DrawLine(screen, a.x, a.y, b.x, b.y, color.White)
 }
 
 //-------------------------Functions----------------------------------
 
-func (c *camera) moveRight(){
-	fmt.Println("move right")
+func updatePoint(p point, cam camera, cp point) point {
+
+	//adding cube center coordinates to point
+	p = add(p, cp)
+
+	//subtracting camera position from point
+	p = subtract(p, cam.pos)
+
+	//point rotation by camera
+	p.rotateX(-cam.rot.x)
+	p.rotateY(-cam.rot.y)
+	p.rotateZ(-cam.rot.z)
+
+	//central projections
+	k := float64(250)
+	centralProjection(&p, k)
+
+	//p.y *= -1
+
+	//moving point to center
+	p.x += sW / 2
+	p.y += sH / 2
+
+	return p
 }
 
-//central projection
-func cproj(p *point, cp point, k float64) {
+func add(a, b point) (res point) {
+	res.x = a.x + b.x
+	res.y = a.y + b.y
+	res.z = a.z + b.z
+	return res
+}
+
+func subtract(a, b point) (res point) {
+	res.x = a.x - b.x
+	res.y = a.y - b.y
+	res.z = a.z - b.z
+	return res
+}
+
+func centralProjection(p *point, k float64) {
 	//k - scaling koefficient
 
 	//formulas
-	z1 := p.z + k
-	x1 := (p.x / z1) * k
-	y1 := (p.y / z1) * k
+	x1 := (p.x / p.z) * k
+	y1 := (p.y / p.z) * k
 
 	p.x, p.y = x1, y1
+}
+
+//rotates point around X axis
+func (p *point) rotateX(angle float64) {
+
+	p.y = (p.y)*math.Cos(angle) - (p.z)*math.Sin(angle)
+	p.z = (p.y)*math.Sin(angle) + (p.z)*math.Cos(angle)
+
+}
+
+//rotates point around Y axis
+func (p *point) rotateY(angle float64) {
+
+	p.x = (p.x)*math.Cos(angle) + (p.z)*math.Sin(angle)
+	p.z = (p.x)*-math.Sin(angle) + (p.z)*math.Cos(angle)
+
+}
+
+//rotates point around Z axis
+func (p *point) rotateZ(angle float64) {
+
+	p.x = (p.x)*math.Cos(angle) - (p.y)*math.Sin(angle)
+	p.y = (p.x)*math.Sin(angle) + (p.y)*math.Cos(angle)
+
 }
 
 //---------------------------Main-------------------------------------
@@ -92,7 +182,6 @@ func main() {
 	//Window
 	ebiten.SetWindowSize(sW, sH)
 	ebiten.SetWindowTitle("Cube x Camera")
-	ebiten.SetWindowResizable(true) //enablening window resize
 
 	//Game instance
 	g := NewGame(sW, sH)                      //creating game instance
@@ -105,8 +194,9 @@ func main() {
 func NewGame(width, height int) *Game {
 
 	//GAME
-	//center point
-	cp := point{sW / 2, sH / 2, 100}
+
+	//cube center point
+	cp := point{0, 0, 300}
 
 	//cube points
 	cube := [8]point{
@@ -122,14 +212,13 @@ func NewGame(width, height int) *Game {
 	}
 
 	edges := [][2]int{
-		{0,1}, {1,2}, {2,3}, {3,0},
-		{4,5}, {5,6}, {6,7}, {7,4},
-		{0,4}, {1,5}, {2,6}, {3,7},
+		{0, 1}, {1, 2}, {2, 3}, {3, 0},
+		{4, 5}, {5, 6}, {6, 7}, {7, 4},
+		{0, 4}, {1, 5}, {2, 6}, {3, 7},
 	}
 
-
 	//CAMERA
-	cam := camera{sW/2, sH/2, 0}
+	cam := camera{pos: point{0, 0, 0}, rot: point{0, 0, 0}}
 
 	//RETURN
 	return &Game{width: width, height: height, cp: cp, cube: cube, edges: edges, cam: cam}
